756b7e0a7c51ca21ef28a33c85bfee32d7c1c108
shivasj <shivasj@gmail.com>
2025-11-29 09:11:09 -0500

    testing /deploy-app

--- src/deployment/deploy-k8s-update.sh
+++ src/deployment/deploy-k8s-update.sh
@@ -1,8 +1,8 @@
 cd deploy_images
-pulumi stack select dev
-pulumi up --stack dev -y
+uv run pulumi stack select dev
+uv run pulumi up --stack dev -y
 
 cd ..
 cd deploy_k8s
-pulumi stack select dev
-pulumi up --stack dev -y
+uv run pulumi stack select dev
+uv run pulumi up --stack dev -y
