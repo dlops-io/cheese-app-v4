df668b2d608baef124007c3f71779e57a79dc81b
shivasj <shivasj@gmail.com>
2025-11-29 11:32:21 -0500

    added ml workflow files

---
+++ src/deployment/deploy-ml-images.sh
@@ -0,0 +1,3 @@
+cd deploy_images_ml
+pulumi stack select dev
+pulumi up --stack dev -y
---
+++ src/workflow/docker-shell.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# set -e
+
+export IMAGE_NAME="cheese-app-workflow"
+export BASE_DIR=$(pwd)
+export SECRETS_DIR=$(pwd)/../../../secrets/
+export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="cheese-app-ml-workflow-demo"
+export GCS_SERVICE_ACCOUNT="ml-workflow@ac215-project.iam.gserviceaccount.com"
+export GCP_REGION="us-central1"
+export GCS_PACKAGE_URI="gs://cheese-app-trainer-code"
+export PULUMI_BUCKET="$GCP_PROJECT-pulumi-state-bucket"
+
+# Build the image based on the Dockerfile
+#docker build -t $IMAGE_NAME -f Dockerfile .
+docker build -t $IMAGE_NAME --platform=linux/amd64 -f Dockerfile .
+
+
+# Run Container
+docker run --rm --name $IMAGE_NAME -ti \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v "$BASE_DIR":/app \
+-v "$SECRETS_DIR":/secrets \
+-v "$BASE_DIR/../data-collector":/data-collector \
+-v "$BASE_DIR/../data-processor":/data-processor \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/ml-workflow.json \
+-e GCP_PROJECT=$GCP_PROJECT \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+-e GCS_SERVICE_ACCOUNT=$GCS_SERVICE_ACCOUNT \
+-e GCP_REGION=$GCP_REGION \
+-e GCS_PACKAGE_URI=$GCS_PACKAGE_URI \
+-e WANDB_KEY=$WANDB_KEY \
+-e PULUMI_BUCKET=$PULUMI_BUCKET \
+$IMAGE_NAME
+
